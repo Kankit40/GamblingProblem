cca9aed4a7c2fb2cc69befb7abf73712cb97ce1c
Kankit40 <Kankit40@gmail.com>
2022-11-12 22:41:29 +0530

    UC4: Code to play the game for 20 days

--- src/com/bridgelabz/Gambling.java
+++ src/com/bridgelabz/Gambling.java
@@ -3,28 +3,34 @@ package com.bridgelabz;
 import java.util.Stack;
 
 public class Gambling {
-    static int STAKE = 100;
     static int BET = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Game");
-        int totalAmount = 100;
-        int play = 0;
 
-        while (totalAmount >50 && totalAmount<150) {
-            play++;
-            int out = (int) (Math.random() * 2);
-            if (out == 1) {
-                System.out.println("You won the game");
-                totalAmount = totalAmount +  BET;
-                System.out.println("Final balance is " + totalAmount);
-            } else {
-                System.out.println("You lost the game");
-                totalAmount = totalAmount - BET;
-                System.out.println("Final balance is " + totalAmount);
+
+        for (int i = 0; i < 21; i++) {
+
+            int totalAmount = 100;
+            int play = 0;
+
+            while (totalAmount > 50 && totalAmount < 150) {
+                play++;
+                int out = (int) (Math.random() * 2);
+                if (out == 1) {
+                    totalAmount = totalAmount + BET;
+                } else {
+                    totalAmount = totalAmount - BET;
+                }
             }
-
+            int max = totalAmount;
+            System.out.println("The number of time played are " + play);
+            System.out.println("Total amount in hand " + totalAmount + " on day " + i);
+            if (max > 100) {
+                System.out.println("You won with 50$");
+            } else {
+                System.out.println("You lost with 50%");
+            }
         }
-        System.out.println("The number of time played are " + play);
     }
 }
